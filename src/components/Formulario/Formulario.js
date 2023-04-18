5beb017184094d5d277e92de7e6dc1d7b4e8c687
Pitelom18 <pitelom18@gmail.com>
2023-04-17 19:58:22 -0500

    se agrega componente header y se adelanta  formulario

---
+++ src/components/Formulario/Formulario.js
@@ -0,0 +1,13 @@
+import "./Formulario.css"
+
+const Formulario = () => {
+    return (
+        <section>
+            <form className="formulario">
+                <h2>Rellena el formulario para crear el colaborador</h2>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
